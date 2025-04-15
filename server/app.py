ddaf3b229b49d8a03c4da0f0a7df0f23efe4fffd
tillenotuoma <totuoma27@gmail.com>
2025-04-15 23:54:12 +0300

    done

--- server/app.py
+++ server/app.py
@@ -19,25 +19,46 @@ class Signup(Resource):
     
     def post(self):
         json = request.get_json()
-        user = User(
-            username=json['username']
-        )
+        user = User(username=json['username'])
         user.password_hash = json['password']
+
         db.session.add(user)
         db.session.commit()
         return user.to_dict(), 201
-
+    
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+
+        if user:
+            return user.to_dict()
+        else:
+            return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        username = request.get_json()['username']
+        password = request.get_json()['password'] 
+
+        user = User.query.filter(User.username == username).first()
+
+        if user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        
+        return {'error': '401 Unauthorized'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='checksession')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+    
